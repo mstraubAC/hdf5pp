e227cd1b6affeedc8b41bd686a131feb8fd1b949
Marcel Straub <straub@physik.rwth-aachen.de>
2013-07-17 17:08:22 +0200

    Added simple exception class

---
+++ Exception.h
@@ -0,0 +1,42 @@
+/*
+ * Exception.h
+ *
+ *  Created on: Jul 17, 2013
+ *      Author: marcel
+ */
+
+#ifndef EXCEPTION_H_
+#define EXCEPTION_H_
+
+#include <exception>
+#include <string>
+#include <sstream>
+
+namespace hdf5
+{
+
+	class Exception: public std::exception
+	{
+		public:
+			Exception() throw(): msg(""), id(0) {};
+			Exception(const std::string& msg, unsigned int id = 0) throw() : msg(msg), id(id) {};
+			Exception(const std::stringstream& msg, unsigned int id = 0) throw() : msg(msg.str()), id(id) {};
+			virtual ~Exception() throw() {};
+
+			Exception& operator=(const Exception& original) throw() {
+				if (this != &original) {
+					msg = original.msg;
+					id = original.id;
+				}
+				return *this;
+			}
+
+			const char* what() const throw() { return msg.c_str(); }
+
+		private:
+			std::string msg;
+			unsigned int id;
+	};
+
+} /* namespace hdf5 */
+#endif /* EXCEPTION_H_ */
